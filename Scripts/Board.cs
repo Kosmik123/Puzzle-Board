02a6aca48e59ecf2a471f1a7f2117799807baf47
Michal <malpa.w.klapkach@gmail.com>
2024-03-25 23:13:25 +0100

    Move board helper to separate file

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -83,25 +83,4 @@ namespace Bipolar.PuzzleBoard
             return piece;
         }
     }
-
-    public static class BoardHelper
-    {
-        public static Vector2Int GetFixedDirection(Vector2Int coord, Vector2Int direction, bool isHexagonal)
-        {
-            if (isHexagonal && direction.y != 0)
-            {
-                if (coord.y % 2 == 0)
-                {
-                    if (direction.x > 0)
-                        direction.x = 0;
-                }
-                else
-                {
-                    if (direction.x <= 0)
-                        direction.x += 1;
-                }
-            }
-            return direction;
-        }
-    }
 }
---
+++ Scripts/BoardHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Bipolar.PuzzleBoard
+{
+    public static class BoardHelper
+    {
+        public static Vector2Int GetFixedDirection(Vector2Int coord, Vector2Int direction, bool isHexagonal)
+        {
+            if (isHexagonal && direction.y != 0)
+            {
+                if (coord.y % 2 == 0)
+                {
+                    if (direction.x > 0)
+                        direction.x = 0;
+                }
+                else
+                {
+                    if (direction.x <= 0)
+                        direction.x += 1;
+                }
+            }
+            return direction;
+        }
+    }
+}
