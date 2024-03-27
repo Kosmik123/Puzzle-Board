93d1050cdfb99fa1b9b8de346019bc9ea257552f
Michal <malpa.w.klapkach@gmail.com>
2024-03-27 13:16:04 +0100

    Simplify some things related to pieces visuals

---
+++ Scripts/IPieceType.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Bipolar.PuzzleBoard
+{
+    public interface IPieceType
+    { }
+
+    public interface ISpritePieceType : IPieceType
+    {
+        Sprite Sprite { get; }
+        Color Color { get; }
+    }
+}
---
+++ Scripts/SpritePieceType.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Bipolar.PuzzleBoard
+{
+    [CreateAssetMenu(menuName = CreateAssetsPath.Root + "Sprite Piece Type")]
+    public class SpritePieceType : PieceType, ISpritePieceType
+    {
+        [SerializeField]
+        private Sprite sprite;
+        public Sprite Sprite => sprite;
+
+        [SerializeField]
+        private Color spriteColor;
+        public Color Color => spriteColor;
+    }
+}
+
