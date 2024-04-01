ad3c8bcf1ba6799a795e4c14095078e60d2c9f33
Michal <malpa.w.klapkach@gmail.com>
2024-04-01 17:49:23 +0200

    Add shuffle to Board Controlller

--- Scripts/BoardController.cs
+++ Scripts/BoardController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Bipolar.PuzzleBoard
 {
@@ -87,6 +88,39 @@ namespace Bipolar.PuzzleBoard
         {
             if (collapseOnStart)
                 Collapse();
+        }
+
+        private readonly LinkedList<Vector2Int> shuffledCoords = new LinkedList<Vector2Int>();
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ShufflePieces();
+            }
+        }
+
+        public void ShufflePieces()
+        {
+            shuffledCoords.Clear();
+            foreach (var coord in Board)
+            {
+                if (Random.value > 0.5f)
+                {
+                    shuffledCoords.AddFirst(coord);
+                }
+                else
+                {
+                    shuffledCoords.AddLast(coord);
+                }
+            }
+
+            foreach (var coord in Board)
+            {
+                var randomCoord = shuffledCoords.First;
+                shuffledCoords.RemoveFirst();
+                (Pieces[randomCoord.Value], Pieces[coord]) = (Pieces[coord], Pieces[randomCoord.Value]);
+            }
         }
 
         public class BoardControllerPiecesIndexable : IPiecesIndexable
