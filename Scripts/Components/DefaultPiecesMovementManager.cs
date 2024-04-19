ed9ecd0ee864c8560db6dc364871dcf7b5a9be80
Michal <malpa.w.klapkach@gmail.com>
2024-04-19 16:47:55 +0200

    Create PieceMovementBehavior (queueing targets)

--- Scripts/Components/DefaultPiecesMovementManager.cs
+++ Scripts/Components/DefaultPiecesMovementManager.cs
@@ -16,10 +16,8 @@ namespace Bipolar.PuzzleBoard.Components
         [SerializeField]
         private float defaultMovementDuration = 0.3f;
 
-        private readonly Dictionary<PieceComponent, Coroutine> pieceMovementCoroutines = new Dictionary<PieceComponent, Coroutine>();
-        public override bool ArePiecesMoving => pieceMovements.Count > 0;
-
-        private readonly Dictionary<PieceComponent, (Vector3 Target, float Speed)> pieceMovements = new Dictionary<PieceComponent, (Vector3 Target, float Speed)>();
+        public override bool ArePiecesMoving => movingPieces.Count > 0;
+        private readonly List<PieceMovementBehavior> movingPieces = new List<PieceMovementBehavior>();
 
         public void StartPieceMovement(PieceComponent piece, Vector2Int targetCoord, float duration = -1)
         {
@@ -27,39 +25,58 @@ namespace Bipolar.PuzzleBoard.Components
                 duration = defaultMovementDuration;
 
             var target = board.CoordToWorld(targetCoord);
-            float distance = Vector3.Distance(piece.transform.position, target);
-            float speed = distance / duration;
-            pieceMovements[piece] = (target, speed);
-        }
-
-        private List<PieceComponent> stoppedPieces = new List<PieceComponent>();
-        private void LateUpdate()
-        {
-            float dt = Time.deltaTime;
-            bool pieceMoved = false;
-            foreach (var pieceMovement in pieceMovements)
+            if (piece.TryGetComponent<PieceMovementBehavior>(out var pieceMovement) && pieceMovement.isActiveAndEnabled)
             {
-                pieceMoved = true;
-                float distance = dt * pieceMovement.Value.Speed;
-                MovePiece(pieceMovement.Key, pieceMovement.Value.Target, distance);
+                float distance = Vector3.Distance(piece.transform.position, target);
+                float speed = distance / duration;
+                movingPieces.Add(pieceMovement);
+                pieceMovement.OnMovementEnded -= PieceMovement_OnMovementEnded;
+                pieceMovement.OnMovementEnded += PieceMovement_OnMovementEnded;
+                pieceMovement.MoveTo(target, speed);
             }
-
-            if (pieceMoved)
+            else
             {
-                foreach (var piece in stoppedPieces)
-                    pieceMovements.Remove(piece);
-
-                stoppedPieces.Clear();
-                if (ArePiecesMoving == false)
-                    OnAllPiecesMovementStopped?.Invoke();
+                piece.transform.position = target;
+                OnPieceMovementEnded?.Invoke(piece);
             }
         }
 
-        private void MovePiece(PieceComponent piece, Vector3 target, float distace)
+        private void PieceMovement_OnMovementEnded(PieceMovementBehavior pieceMovement)
         {
-            piece.transform.position = Vector3.MoveTowards(piece.transform.position, target, distace);
-            if (piece.transform.position == target)
-                stoppedPieces.Add(piece);
+            pieceMovement.OnMovementEnded -= PieceMovement_OnMovementEnded;
+            movingPieces.Remove(pieceMovement);
+            if (ArePiecesMoving == false)
+                OnAllPiecesMovementStopped?.Invoke();
         }
+
+        //private List<PieceComponent> stoppedPieces = new List<PieceComponent>();
+        //private void LateUpdate()
+        //{
+        //    float dt = Time.deltaTime;
+        //    bool pieceMoved = false;
+        //    foreach (var pieceMovement in pieceMovements)
+        //    {
+        //        pieceMoved = true;
+        //        float distance = dt * pieceMovement.Value.Speed;
+        //        MovePiece(pieceMovement.Key, pieceMovement.Value.Target, distance);
+        //    }
+
+        //    if (pieceMoved)
+        //    {
+        //        foreach (var piece in stoppedPieces)
+        //            pieceMovements.Remove(piece);
+
+        //        stoppedPieces.Clear();
+        //        if (ArePiecesMoving == false)
+        //            OnAllPiecesMovementStopped?.Invoke();
+        //    }
+        //}
+
+        //private void MovePiece(PieceComponent piece, Vector3 target, float distace)
+        //{
+        //    piece.transform.position = Vector3.MoveTowards(piece.transform.position, target, distace);
+        //    if (piece.transform.position == target)
+        //        stoppedPieces.Add(piece);
+        //}
     }
 }
---
+++ Scripts/Components/PieceMovementBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Bipolar.PuzzleBoard.Components
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(PieceComponent))]
+    public abstract class PieceMovementBehavior : MonoBehaviour
+    {
+        public abstract event System.Action<PieceMovementBehavior> OnMovementEnded;
+
+        public abstract void MoveTo(Vector3 targetPosition, float speed = -1);
+    }
+}
